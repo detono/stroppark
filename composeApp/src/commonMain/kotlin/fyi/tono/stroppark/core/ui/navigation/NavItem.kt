098fafad07097d424f2be4f4cc0292e53b605dee
Tono <tono@instamobile.be>
2026-03-11 11:16:50 +0100

    refactor(ui): updated icon

--- composeApp/src/commonMain/kotlin/fyi/tono/stroppark/core/ui/navigation/NavItem.kt
+++ composeApp/src/commonMain/kotlin/fyi/tono/stroppark/core/ui/navigation/NavItem.kt
@@ -2,10 +2,8 @@ package fyi.tono.stroppark.core.ui.navigation
 
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ElectricalServices
-import androidx.compose.material.icons.filled.Garage
+import androidx.compose.material.icons.filled.EvStation
 import androidx.compose.material.icons.filled.LocalParking
-import androidx.compose.material.icons.filled.Place
 import androidx.compose.ui.graphics.vector.ImageVector
 
 
@@ -23,6 +21,6 @@ sealed class NavItem(
   object Chargers : NavItem(
     route = "chargers",
     title = "Chargers",
-    icon = Icons.Filled.ElectricalServices
+    icon = Icons.Filled.EvStation
   )
 }
