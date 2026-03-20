0c263beed5254264b822f226aea5449378501b37
Tono <tono@instamobile.be>
2026-03-20 14:53:54 +0100

    perf(ktor): set timeout for ktor requests

--- composeApp/src/commonMain/kotlin/fyi/tono/stroppark/core/network/KtorClient.kt
+++ composeApp/src/commonMain/kotlin/fyi/tono/stroppark/core/network/KtorClient.kt
@@ -1,6 +1,7 @@
 package fyi.tono.stroppark.core.network
 
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
@@ -11,7 +12,11 @@ fun createHttpClient(json: Json) = HttpClient {
   install(ContentNegotiation) {
     json(json)
   }
-
+  install(HttpTimeout) {
+    connectTimeoutMillis = 10_000
+    requestTimeoutMillis = 30_000
+    socketTimeoutMillis = 30_000
+  }
   install(Logging) {
     logger = object : Logger {
       override fun log(message: String) {
