bd28a7259a3fc19649b2a58ee1bd8ffcd1a2d8bc
Tono <tono@instamobile.be>
2026-03-25 17:29:55 +0100

    feat: follow user's continuously

--- composeApp/src/commonMain/kotlin/fyi/tono/stroppark/core/location/LocationServiceImpl.kt
+++ composeApp/src/commonMain/kotlin/fyi/tono/stroppark/core/location/LocationServiceImpl.kt
@@ -1,7 +1,9 @@
 package fyi.tono.stroppark.core.location
 
 import fyi.tono.stroppark.core.network.dto.GhentCoordinatesDto
+import kotlinx.coroutines.flow.Flow
 
 expect class LocationServiceImpl: LocationService {
   override suspend fun getCurrentLocation(): GhentCoordinatesDto?
+  override fun getLocationFlow(): Flow<GhentCoordinatesDto?>
 }
