93036b353e96c5bc82a7b4b2d02625a4feb3534d
Tono <tono@instamobile.be>
2026-03-25 13:43:25 +0100

    chore: add SnackbarHotsState via CompositionLocal

--- composeApp/src/commonMain/kotlin/fyi/tono/stroppark/core/ui/navigation/AppNavigation.kt
+++ composeApp/src/commonMain/kotlin/fyi/tono/stroppark/core/ui/navigation/AppNavigation.kt
@@ -1,22 +1,18 @@
 package fyi.tono.stroppark.core.ui.navigation
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavDestination.Companion.hierarchy
-import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import fyi.tono.stroppark.core.utils.LocalSnackbar
 import fyi.tono.stroppark.features.chargers.ui.screens.ChargerListScreen
 import fyi.tono.stroppark.features.parking.ui.screens.ParkingListScreen
 
@@ -26,25 +22,33 @@ fun AppNavigation(
   modifier: Modifier = Modifier,
   navController: NavHostController = rememberNavController()
 ) {
+  val snackBarHostState = remember { SnackbarHostState() }
 
-  Scaffold(
-    bottomBar = {
-      StropParkBottomBar(navController = navController)
-    },
-    content = { innerPadding ->
-      NavHost(
-        navController = navController,
-        startDestination = NavItem.Parking.route,
-        modifier = modifier.padding(innerPadding),
-        builder = {
-          composable(NavItem.Parking.route) {
-            ParkingListScreen()
+  CompositionLocalProvider(value = LocalSnackbar provides snackBarHostState) {
+    Scaffold(
+      snackbarHost = {
+        SnackbarHost(
+          hostState = snackBarHostState
+        )
+      },
+      bottomBar = {
+        StropParkBottomBar(navController = navController)
+      },
+      content = { innerPadding ->
+        NavHost(
+          navController = navController,
+          startDestination = NavItem.Parking.route,
+          modifier = modifier.padding(innerPadding),
+          builder = {
+            composable(NavItem.Parking.route) {
+              ParkingListScreen()
+            }
+            composable(NavItem.Chargers.route) {
+              ChargerListScreen()
+            }
           }
-          composable(NavItem.Chargers.route) {
-            ChargerListScreen()
-          }
-        }
-      )
-    }
-  )
+        )
+      }
+    )
+  }
 }
