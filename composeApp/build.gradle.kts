bb7849db1ae2ffa5a39363fd161752571e5e0dcc
Tono <tono@instamobile.be>
2026-03-26 11:48:49 +0100

    build: enable minify & shrink again for debug

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -183,8 +183,8 @@ android {
     }
     buildTypes {
         debug {
-            isMinifyEnabled = false
-            isShrinkResources = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
