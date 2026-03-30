598e43049ac53a2436ace3435c4c5c9dc1ac5a44
Tono <jonathan.tono@gmail.com>
2026-03-30 11:08:21 +0200

    fix: get last known location as first location instead of null for better user experience

--- composeApp/src/commonMain/kotlin/fyi/tono/stroppark/core/location/LocationService.kt
+++ composeApp/src/commonMain/kotlin/fyi/tono/stroppark/core/location/LocationService.kt
@@ -4,6 +4,7 @@ import fyi.tono.stroppark.core.network.dto.GhentCoordinatesDto
 import kotlinx.coroutines.flow.Flow
 
 interface LocationService {
+  suspend fun getLastKnownLocation(): GhentCoordinatesDto?
   suspend fun getCurrentLocation(): GhentCoordinatesDto?
   fun getLocationFlow(): Flow<GhentCoordinatesDto?>
 }
